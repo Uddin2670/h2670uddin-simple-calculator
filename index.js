bd66b43770b4c429fb1bca23b18c27f4c15bd08c
Uddin2670 <Hamzauddin700@gmail.com>
2024-03-21 15:27:04 -0700

    typetojava

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "select one of the operators to perform action,",
         type: "list",
@@ -26,3 +26,4 @@ else if (answer.operator === "Division") {
 else {
     console.log("please select value operator");
 }
+console.log("THE END");
